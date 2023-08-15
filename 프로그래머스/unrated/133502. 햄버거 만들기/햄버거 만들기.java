492ea9afecd5feb34b2a284f910f25b13054348a
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2023-08-16 02:00:24 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.04 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        ArrayList<Integer>arr = new ArrayList<>();
+        for(int i = 0 ;i<ingredient.length;i++){
+            arr.add(ingredient[i]);
+        }
+        for(int i = 3;i<arr.size();i++){
+            if(arr.get(i)==1
+               &&arr.get(i-1)==3
+                &&arr.get(i-2)==2
+              &&arr.get(i-3)==1){
+                answer++;
+                arr.remove(i);
+                arr.remove(i-1);
+                arr.remove(i-2);
+                arr.remove(i-3);
+                i-=4;
+                if(i<3)
+                    i=2;
+            }
+        }
+        
+        return answer;
+    }
+}
