5e8f58083e42ca63b548eefa5c4efe89a8a8ecca
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2023-08-16 02:03:22 +0900

    Update and rename 햄버거 만들기.java

--- 프로그래머스/lv1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/lv1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -7,21 +7,14 @@ class Solution {
             arr.add(ingredient[i]);
         }
         for(int i = 3;i<arr.size();i++){
-            if(arr.get(i)==1
-               &&arr.get(i-1)==3
-                &&arr.get(i-2)==2
-              &&arr.get(i-3)==1){
+            if(arr.get(i)==1&&arr.get(i-1)==3&&arr.get(i-2)==2&&arr.get(i-3)==1){
                 answer++;
-                arr.remove(i);
-                arr.remove(i-1);
-                arr.remove(i-2);
-                arr.remove(i-3);
+                arr.remove(i);arr.remove(i-1);
+                arr.remove(i-2);arr.remove(i-3);
                 i-=4;
-                if(i<3)
-                    i=2;
+                if(i<3) i=2;
             }
         }
-        
         return answer;
     }
 }
