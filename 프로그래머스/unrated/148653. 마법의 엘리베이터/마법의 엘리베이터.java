8f0935658c6793e9745512e6161c537ef34db2bd
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2023-09-06 15:37:54 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,29 +1,30 @@
 class Solution {
     public int solution(int storey) {
-        int answer1 = 0;
-        int storey1 = storey;
+        int answer = 0;
         while(storey>0){
-            if(storey%10>5){
-                answer1+=(10 - storey%10);
-                storey/=10;
-                storey++;
-            }else{
-                answer1+=(storey%10);
-                storey/=10;
+            if(((storey%100)/10)<5){
+            	if(storey%10>5) {
+            		answer+=10-storey%10;
+            		storey+=10;
+            	}else if(storey%10<5) {
+            		answer+=storey%10;
+            	}else {
+            		answer+=storey%10;
+            	}
+            }else {
+            	if(storey%10>5) {
+            		answer+=10-storey%10;
+            		storey+=10;
+            	}else if(storey%10<5) {
+            		answer+=storey%10;
+            	}else {
+            		answer+=10-storey%10;
+            		storey+=10;
+            	}
             }
-        }
-        int answer2 = 0;
-        while(storey1>0){
-            if(storey1%10>=5){
-                answer2+=(10 - storey1%10);
-                storey1/=10;
-                storey1++;
-            }else{
-                answer2+=(storey1%10);
-                storey1/=10;
-            }
+            storey/=10;
         }
         
-        return answer1>answer2?answer2:answer1;
+        return answer;
     }
 }
