ce7357678ed130c2dcb62d11a8ac9c90d723c56d
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2023-10-03 15:43:23 +0900

    [D3] Title: 원 안의 점, Time: 158 ms, Memory: 23,004 KB -BaekjoonHub

---
+++ SWEA/D3/16910. 원 안의 점/원 안의 점.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int num = 0;
+			int r = sc.nextInt();
+			for(double x = 0;x<=r;x++) {
+				num+= (int)Math.sqrt(r*r - x*x);
+			}
+			System.out.printf("#%d %d\n",test_case,4*num+1);
+		}
+	}
+}
