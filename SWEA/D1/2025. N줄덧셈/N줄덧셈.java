b1c0a289d39d660b086aed3eefa76015359e5470
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2023-11-16 19:07:58 +0900

    [D1] Title: N줄덧셈, Time: 131 ms, Memory: 20,468 KB -BaekjoonHub

---
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class Solution {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println((n+1)*n/2);
+	}
+}
