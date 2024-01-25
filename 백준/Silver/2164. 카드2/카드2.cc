90bafc899c7997c7603615b586079d527bac405b
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-01-25 19:49:31 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -12,21 +12,13 @@ int main() {
 	cin >> N;
 
 	queue <int> result;
-	bool th = true;
 	for (int i = 1; i <= N; i++) {
 		result.push(i);
 	}
 	while (result.size() > 1) {
-		if (th) {
-			result.pop();
-			th = false;
-		}
-		else {
-			int num = result.front();
-			result.pop();
-			result.push(num);
-			th = true;
-		}
+		result.pop();
+		result.push(result.front());
+		result.pop();
 	}
-	cout << result.front() << "\n";
+	cout << result.front();
 }
