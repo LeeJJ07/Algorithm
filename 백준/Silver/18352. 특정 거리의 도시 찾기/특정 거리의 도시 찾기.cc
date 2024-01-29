28f5768a129b227fcca491bdd4e5f90f6aa3bf8e
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-01-30 00:49:16 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 372 ms, Memory: 21724 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -2,13 +2,12 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
-
 using namespace std;
 
 void BFS(int node);
 static vector<vector<int>> A;
-static vector<int> answer;
-static vector<int> visited;
+static vector<int>result;
+static vector<int>visited;
 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -18,11 +17,13 @@ int main() {
 
 	A.resize(N + 1);
 
-	for (int i = 0; i < M; i++) {
-		int s, e;
-		cin >> s >> e;
-		A[s].push_back(e);
+	for (int i = 0; i < M; i++)
+	{
+		int S, E;
+		cin >> S >> E;
+		A[S].push_back(E);
 	}
+
 	visited.resize(N + 1);
 	for (int i = 0; i <= N; i++) {
 		visited[i] = -1;
@@ -32,35 +33,33 @@ int main() {
 
 	for (int i = 1; i <= N; i++) {
 		if (visited[i] == K) {
-			answer.push_back(i);
+			result.push_back(i);
 		}
 	}
-	sort(answer.begin(), answer.end());
 
-	if (answer.empty()) {
+	if (result.empty()) {
 		cout << -1 << "\n";
 	}
 	else {
-		sort(answer.begin(), answer.end());
-		for (int temp : answer) {
+		sort(result.begin(), result.end());
+		for (int temp : result) {
 			cout << temp << "\n";
 		}
 	}
 }
-
 void BFS(int node) {
 	queue<int> q;
 	q.push(node);
 	visited[node]++;
 
 	while (!q.empty()) {
-		int now_node = q.front();
+		int new_node = q.front();
 		q.pop();
-		for (int i : A[now_node]) {
-			if (visited[i] == -1) {
-				visited[i] = visited[now_node] + 1;
-				q.push(i);
-			}
+		for (int i : A[new_node]) {
+			if (visited[i] != -1)
+				continue;
+			q.push(i);
+			visited[i] = visited[new_node] + 1;
 		}
 	}
 }
