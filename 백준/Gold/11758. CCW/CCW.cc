9a3d2d37878143f7550caad563ee0f22b203b785
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-02-01 15:46:56 +0900

    [Gold V] Title: CCW, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int x1, y1, x2, y2, x3, y3;
+	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+	int CCW = (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3);
+	if (CCW > 0)
+		cout << 1 << "\n";
+	else if (CCW < 0)
+		cout << -1 << "\n";
+	else
+		cout << 0 << "\n";
+}
