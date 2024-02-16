abb6dc681125977d6b43068d7617dc1373d63cea
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-02-16 19:10:04 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,29 +1,16 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
-
-bool isPD(string input) {
-	int mid = input.length() / 2;
-
-	for (int i = 0; i < mid; i++) {
-		if (input[i] != input[input.length()-1-i]) {
-			return false;
-		}
-	}
-	return true;
-}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	string str;
-	cin >> str;
-	if (isPD(str)) {
-		cout << 1 << '\n';
-	}
-	else {
-		cout << 0 << '\n';
-	}
+	string s;
+	cin >> s;
+	string temp = s;
+	reverse(temp.begin(), temp.end());
+	if (temp == s) cout << 1 << '\n';
+	else cout << 0 << '\n';
 }
