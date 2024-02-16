47cc21b86adfad7cad2b161992794258cf6304f6
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-02-16 21:54:45 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -1,21 +1,18 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-	int num;
-	cin >> num;
-
-	string numbers;
-	cin >> numbers;
-
+	int N;
+	cin >> N;
 	int sum = 0;
-
-	for (int i = 0; i < numbers.length(); i++) {
-		sum += numbers[i] - '0';
+	for (int i = 0; i < N; i++) {
+		char c;
+		cin >> c;
+		sum += int(c - '0');
 	}
-	cout << sum << endl;
-
-	return 0;
+	cout << sum << '\n';
 }
