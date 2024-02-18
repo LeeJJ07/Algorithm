54d8813310dd881f121fd38a35141f5a95b7ad32
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-02-18 18:19:07 +0900

    [Silver II] Title: DNA 비밀번호, Time: 16 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
@@ -1,95 +1,73 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int checkResult = 0;
+int ruleDNA[4];
+int myDNA[4];
 
-void Add(char c, int* check, int* my);
-void Remove(char c, int* check, int* my);
+void add(char c) {
+	switch (c) {
+	case 'A':
+		myDNA[0]++;
+		break;
+	case 'C':
+		myDNA[1]++;
+		break; 
+	case 'G':
+		myDNA[2]++;
+		break;
+	case 'T':
+		myDNA[3]++;
+		break;
+	}
+}
+void sub(char c) {
+	switch (c) {
+	case 'A':
+		myDNA[0]--;
+		break;
+	case 'C':
+		myDNA[1]--;
+		break;
+	case 'G':
+		myDNA[2]--;
+		break;
+	case 'T':
+		myDNA[3]--;
+		break;
+	}
+}
+
+bool check() {
+	for (int i = 0; i < 4; i++) {
+		if (ruleDNA[i] > myDNA[i])
+			return false;
+	}
+	return true;
+}
 
 int main() {
-
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N, M;
-	cin >> N >> M;
-
-	string A;
-	cin >> A;
-	int check[4];
-	int my[4] = { 0 };
-
-	int result = 0;
-
+	int S, P;
+	cin >> S >> P;
+	string dna;
+	cin >> dna;
 	for (int i = 0; i < 4; i++) {
-		cin >> check[i];
-		if (check[i] == 0)
-			checkResult++;
+		cin >> ruleDNA[i];
 	}
-	for (int i = 0; i < M; i++) {
-		Add(A[i], check, my);
+	for (int i = 0; i < P; i++) {
+		add(dna[i]);
 	}
-	if (checkResult == 4) {
-		result++;
-	}
+	int result = 0;
+	if (check())result++;
 
-	for (int i = M; i < N; i++) {
-		int j = i - M;
-		Add(A[i],check,my);
-		Remove(A[j], check, my);
-		if (checkResult == 4)
-			result++;
+	for (int i = P; i < S; i++) {
+		sub(dna[i - P]);
+		add(dna[i]);
+		if (check())result++;
 	}
-	cout << result << "\n";
-}
-
-void Add(char c, int* check, int* my) {
-	switch (c) {
-	case 'A':
-		my[0]++;
-		if (my[0] == check[0])
-			checkResult++;
-		break;
-	case 'C':
-		my[1]++;
-		if (my[1] == check[1])
-			checkResult++;
-		break;
-	case 'G':
-		my[2]++;
-		if (my[2] == check[2])
-			checkResult++;
-		break;
-	case 'T':
-		my[3]++;
-		if (my[3] == check[3])
-			checkResult++;
-		break;
-	}
-}
-void Remove(char c, int* check, int* my) {
-	switch (c) {
-	case 'A':
-		if (my[0] == check[0])
-			checkResult--;
-		my[0]--;
-		break;
-	case 'C':
-		if (my[1] == check[1])
-			checkResult--;
-		my[1]--;
-		break;
-	case 'G':
-		if (my[2] == check[2])
-			checkResult--;
-		my[2]--;
-		break;
-	case 'T':
-		if (my[3] == check[3])
-			checkResult--;
-		my[3]--;
-		break;
-	}
+	cout << result << '\n';
 }
