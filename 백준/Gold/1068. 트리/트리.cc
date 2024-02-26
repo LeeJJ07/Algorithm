74eb33481a759d678014fd76494c19bfd07d3596
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-02-26 17:22:56 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -2,50 +2,47 @@
 #include <vector>
 using namespace std;
 
-void DFS(int node);
-static vector<vector<int>> tree;
-static vector<bool> visited;
-static int result;
+vector<vector<int>> tree;
+vector<int> visited;
+int result = 0;
+void DFS(int s) {
+	visited[s] = 1;
+	if (tree[s].empty()) result++;
+	for (int i : tree[s]) {
+		if (visited[i]) continue;
+		DFS(i);
+	}
+}
+
 int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 	int N;
 	cin >> N;
 	tree.resize(N);
 	visited.resize(N);
-	int root;
+	vector<int> temp(N);
+	int root = -1;
 	for (int i = 0; i < N; i++) {
-		int node;
-		cin >> node;
-		if (node == -1) {
+		int p;
+		cin >> p;
+		temp[i] = p;
+	}
+	int del;
+	cin >> del;
+	for (int i = 0; i < N; i++) {
+		if (i == del) continue;
+		if (temp[i] == -1) {
 			root = i;
 			continue;
 		}
-		tree[node].push_back(i);
+		tree[temp[i]].push_back(i);
 	}
-	int era;
-	cin >> era;
-
-	visited[era] = true;
-	
-	result = 0;
-	if (era == root) {
-		cout << 0 << "\n";
-	}
+	if (root == -1) cout << 0 << '\n';
 	else {
 		DFS(root);
-		cout << result << "\n";
+		cout << result << '\n';
 	}
-}
-void DFS(int node) {
-	visited[node] = true;
-	int count = 0;
-	for (int i : tree[node]) {
-		if (visited[i])
-			continue;
-		DFS(i);
-		count++;
-	}
-	if(count==0)
-		result++;
 }
