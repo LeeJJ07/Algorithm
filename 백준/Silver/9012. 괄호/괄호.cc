1b99dcb90d1705484a2566202ca6680e9e46d59d
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-02-26 14:17:42 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -1,43 +1,37 @@
 #include <iostream>
 #include <stack>
-
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	
-	int N;
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		string str;
-		cin >> str;
-		bool result = true;
-		stack<char> myStack;
-		for (int j = 0; j < str.size(); j++) {
-			if (myStack.empty()) {
-				if (str[j] == '(') {
-					myStack.push(str[j]);
-				}
-				else if (str[j] == ')') {
-					result = false;
-					break;
-				}
-			}
-			else {
-				if (str[j] == '(')
-					myStack.push(str[j]);
-				else
-					myStack.pop();
-			}
-		}
-		if (!myStack.empty())
-			result = false;
-		
-		if (result) {
-			cout << "YES" << "\n";
+bool check(string input) {
+	stack<char> s;
+	for (int i = 0; i < input.size(); i++) {
+		if (input[i] == '(') {
+			s.push(input[i]);
 		}
 		else {
-			cout << "NO" << "\n";
+			if (s.empty())
+				return false;
+			if (s.top() == '(')
+				s.pop();
+			else return false;
 		}
+	}
+	if (s.size())
+		return false;
+	return true;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string input;
+		cin >> input;
+		if (check(input)) cout << "YES\n";
+		else cout << "NO\n";
 	}
 }
