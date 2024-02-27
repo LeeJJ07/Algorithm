2324ba0b73891a3452081e43eb75f885b6ffca20
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-02-27 14:27:40 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -1,43 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <stack>
-
+#include <algorithm>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
+	
 	int N;
 	cin >> N;
-	
-	vector <int>A(N,0);
-	for (int i = 0; i < N; i++) {
+	vector<int> A(N+1);
+	int sum = 0;
+	for (int i = 1; i <= N; i++) {
 		cin >> A[i];
 	}
-	
-	for (int i = 1; i < N; i++) {
-		int insert_point = i;
-		int insert_value = A[i];
-		for (int j = i - 1; j >= 0; j--) {
-			if (A[j] < A[i]) {
-				insert_point = j + 1;
-				break;
-			}
-			if (j == 0) {
-				insert_point = 0;
-			}
-		}
-		for (int j = i; j > insert_point; j--) {
-			A[j] = A[j - 1];
-		}
-		A[insert_point] = insert_value;
+	sort(A.begin(), A.end());
+	for (int i = 1; i <= N; i++) {
+		A[i] += A[i - 1];
+		sum += A[i];
 	}
-	int result = A[0];
-	for (int i = 1; i < N; i++) {
-		A[i] += A[i-1];
-		result += A[i];
-	}
-	cout << result;
+	cout << sum << '\n';
 }
