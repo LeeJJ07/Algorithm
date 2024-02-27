6294520194323c1c1a12ca9651519bee4cb06eec
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-02-27 13:54:11 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,25 +1,17 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
+	
 	int N;
 	cin >> N;
-
-	vector <int> A(N, 0);
-	for (int i = 0; i < N; i++) {
-		cin >> A[i];
-	}
-
+	vector<int> A(N);
+	for (int i = 0; i < N; i++) cin >> A[i];
 	sort(A.begin(), A.end());
-	
-	for (int i = 0; i < N; i++) {
-		cout << A[i]<<"\n";
-	}
+	for (int i = 0; i < N; i++) cout << A[i] << '\n';
 }
