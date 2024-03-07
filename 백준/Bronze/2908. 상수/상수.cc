a768712ea0a8790e12addf50c941c8a21714ded8
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-03-07 18:48:43 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	
+	string a, b;
+	cin >> a >> b;
+	reverse(a.begin(), a.end());
+	reverse(b.begin(), b.end());
+	if (a > b) cout << a;
+	else cout << b;
+}
