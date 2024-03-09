886b8725e3bb457e1fc7897d46df149a0fad8d52
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-03-09 11:08:42 +0900

    [Gold IV] Title: 알파벳, Time: 420 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,36 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int R, C;
-char m[24][24];
-int check[26];
-int dy[] = { -1,0,1,0 };
-int dx[] = { 0,1,0,-1 };
+int r, c;
+char a[24][24];
+int visited[26];
+int dy[] = { -1, 0, 1, 0 };
+int dx[] = { 0, 1, 0, -1 };
 int result = 0;
-
-void DFS(int y, int x, int level) {
-	if (result < level) result = level;
-
+void dfs(int y, int x, int depth) {
+	if (result < depth) result = depth;
 	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (ny<1 || nx<1 || ny>R || nx>C || check[m[ny][nx] - 'A'])continue;
-		check[m[ny][nx] - 'A'] = 1;
-		DFS(ny, nx, level + 1);
-		check[m[ny][nx] - 'A'] = 0;
+		int ny = dy[i] + y;
+		int nx = dx[i] + x;
+		if (ny < 0 || nx < 0 || ny >= r || nx >= c || visited[a[ny][nx]-'A']) continue;
+		visited[a[ny][nx]-'A'] = 1;
+		dfs(ny, nx, depth + 1);
+		visited[a[ny][nx]-'A'] = 0;
 	}
 }
-int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	
-	cin >> R >> C;
 
-	for (int i = 1; i <= R; i++) {
-		for (int j = 1; j <= C; j++) {
-			cin >> m[i][j];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> r >> c;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cin >> a[i][j];
 		}
 	}
-	check[m[1][1] - 'A']++;
-	DFS(1, 1, 1);
-	cout << result << '\n';
+	visited[a[0][0] - 'A'] = 1;
+	dfs(0, 0, 1);
+	cout << result;
 }
