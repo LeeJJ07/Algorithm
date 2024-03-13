b76f9da0b7aa5bd503782eac2f29b3709f449820
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-03-13 19:18:44 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    struct tm* t;
+    time_t timer;
+    timer = time(NULL);
+    t = localtime(&timer);
+
+    cout << t->tm_year + 1900 << "-";
+    cout.width(2);
+    cout.fill('0');
+    cout << t->tm_mon + 1 << "-";
+    cout.width(2);
+    cout.fill('0');
+    cout << t->tm_mday;
+    return 0;
+}
