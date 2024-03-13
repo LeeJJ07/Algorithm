bb2a7ef43b9f6dc58acb2fc48a32588a88057b96
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-03-13 15:01:04 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 40 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef pair<int, int> node;
+
+bool cmp(node a, node b) {
+	if (a.second == b.second) return a.first < b.first;
+	return a.second < b.second;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n; cin >> n;
+	vector<node> a(n);
+	for (int i = 0; i < n; i++)
+		cin >> a[i].first >> a[i].second;
+
+	sort(a.begin(), a.end(), cmp);
+	for (node i : a)
+		cout << i.first << ' ' << i.second << '\n';
+}
