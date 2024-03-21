de34069d5f133ab613924edabfd344bcbbb402c1
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-03-21 17:28:03 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 28 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.cc
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.cc
@@ -1,78 +1,90 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
 #include <algorithm>
-#include <cstring>
 using namespace std;
-#define s second
-#define f first
 
-const int INF = 987654321;
-const int dy[] = { 0, 1, 0, -1 };
-const int dx[] = { 1, 0, -1, 0 };
-int n, m, k, a[104][104], b[104][104], ret = INF, r, c, s, visited[104][104], dir, sy, sx, ey, ex;
-vector<pair<int, int>> vv;
-vector<int> v_idx;
-struct A {
-	int y, x, cnt;
-};
-vector<A> v;
-void go(int y, int x, int first) {
-	if (!first && y == sy && x == sx) dir++;
-	if (!first && y == sy && x == ex) dir++;
-	if (!first && y == ey && x == ex) dir++;
-	if (!first && y == ey && x == sx) dir++;
-	int ny = y + dy[dir];
-	int nx = x + dx[dir];
-	if (visited[ny][nx]) return;
-	visited[ny][nx] = 1;
-	vv.push_back({ ny, nx });
-	go(ny, nx, 0);
-}
-void rotateAll(int y, int x, int cnt) {
-	for (int i = 1; i <= cnt; i++) {
-		sy = y - 1 * i;
-		sx = x - 1 * i;
-		ey = y + 1 * i;
-		ex = x + 1 * i;
-		vv.clear();
-		dir = 0;
-		memset(visited, 0, sizeof(visited));
-		visited[sy][sx] = 1;
-		vv.push_back({ sy, sx });
-		go(sy, sx, 1);
+int dy[] = { 0, 1, 0, -1 };
+int dx[] = { 1, 0, -1, 0 };
 
-		vector<int> vvv;
-		for (pair<int, int> c : vv)vvv.push_back(b[c.f][c.s]);
-		rotate(vvv.rbegin(), vvv.rbegin() + 1, vvv.rend());
-		for (int i = 0; i < vv.size(); i++) b[vv[i].f][vv[i].s] = vvv[i];
+int n, m, k, r, c, s;
+int a[54][54];
+int temp[54][54];
+
+void _turn_all(int y, int x, int sz) {
+	int ny = y, nx = x;
+	int idx = 0;
+	vector<int> tmp((sz - 1) * 4);
+	for (int i = 0; i < 4; i++) {
+		while (idx<tmp.size() && ny >= y && nx >= x && ny <= y + sz - 1 && nx <= x + sz - 1) {
+			tmp[idx++] = temp[ny][nx];
+			ny += dy[i];
+			nx += dx[i];
+		}
+		ny -= dy[i];
+		nx -= dx[i];
+		idx--;
+	}
+	int tp = tmp[tmp.size() - 1];
+	for (int i = tmp.size()-1; i >0; i--) tmp[i] = tmp[i - 1];
+	tmp[0] = tp;
+	ny = y, nx = x;
+	idx = 0;
+	for (int i = 0; i < 4; i++) {
+		while (idx < tmp.size() && ny >= y && nx >= x && ny <= y + sz - 1 && nx <= x + sz - 1) {
+			temp[ny][nx] = tmp[idx++];
+			ny += dy[i];
+			nx += dx[i];
+		}
+		ny -= dy[i];
+		nx -= dx[i];
+		idx--;
 	}
 }
-int solve() {
-	for (int i : v_idx) rotateAll(v[i].y, v[i].x, v[i].cnt);
-	int mn = INF;
-	for (int i = 0; i < n; i++) {
-		int cnt = 0;
-		for (int j = 0; j < m; j++)cnt += b[i][j];
-		mn = min(mn, cnt);
-	}
-	return mn;
-}
+
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
 	cin >> n >> m >> k;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
 			cin >> a[i][j];
 		}
 	}
+	vector<int> v;
+	vector<tuple<int, int, int>> vv;
 	for (int i = 0; i < k; i++) {
-		cin >> r >> c >> s; r--; c--;
-		v.push_back({ r, c, s });
-		v_idx.push_back(i);
+		cin >> r >> c >> s;
+		vv.push_back({r, c, s});
+		v.push_back(i);
 	}
+	int res = 98765432;
 	do {
-		memcpy(b, a, sizeof(b));
-		ret = min(ret, solve());
-	} while (next_permutation(v_idx.begin(), v_idx.end()));
-	cout << ret << "\n";
-	return 0;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				temp[i][j] = a[i][j];
+			}
+		}
+		for (int i = 0; i < k; i++) {
+			s = get<2>(vv[v[i]]);
+			c = get<1>(vv[v[i]]);
+			r = get<0>(vv[v[i]]);
+			int sz = 2 * s + 1, sy = r - s, sx = c - s;
+			while (sz > 1) {
+				_turn_all(sy, sx, sz);
+				sy++; sx++;
+				sz -= 2;
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			int sum = 0;
+			for (int j = 1; j <= m; j++) {
+				sum += temp[i][j];
+			}
+			res = min(res, sum);
+		}
+	} while (next_permutation(v.begin(), v.end()));
+	
+	cout << res << '\n';
 }
