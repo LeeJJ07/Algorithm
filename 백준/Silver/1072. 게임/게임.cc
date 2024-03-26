237005daf3e8b7248d3efe2cd9c6e32b741a9640
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-03-26 20:10:42 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.cc
+++ 백준/Silver/1072. 게임/게임.cc
@@ -1,28 +1,23 @@
 #include <iostream>
 using namespace std;
 typedef long long ll;
-ll x, y;
+ll a, b, mid, ret = -1;
 
-bool check(ll mid) {
-	ll z = y * 100 / x;
-	ll zz = (y + mid) * 100 / (x + mid);
-	return z != zz;
-}
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> x >> y;
-	ll lo = 1, hi = 1e10, mid, res = 1e18;
+	cin >> a >> b;
+	ll z = b * 100 / a;
+	ll lo = 1, hi = 1e9;
 	while (lo <= hi) {
 		mid = (lo + hi) / 2;
-		if (check(mid)) {
-			res = min(res, mid);
+		if ((b + mid) * 100 / (a + mid) > z) {
+			ret = mid;
 			hi = mid - 1;
 		}
 		else lo = mid + 1;
 	}
-	if (res == 1e18) cout << -1 << '\n';
-	else cout << res << '\n';
+	cout << ret << '\n';
 }
