df0730550ada69debc0b25dc965e9dbbe648b5b0
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-04-22 00:34:00 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.cc
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -3,18 +3,15 @@
 #include <algorithm>
 using namespace std;
 
-int n, input;
+int n, i, m;
 vector<int> v;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> n;
-	while (n--) { cin >> input; v.push_back(input); }
-	if (n == 1) cout << input * input << '\n';
-	else {
-		sort(v.begin(), v.end());
-		cout << v[0] * v[v.size() - 1] << '\n';
-	}
+	cin >> n; m = n;
+	while (n--) { cin >> i; v.push_back(i); }
+	sort(v.begin(), v.end());
+	cout << v[0] * v[m - 1];
 }
