8ff7749240d0a77b273e77ebd934870f3fb2feef
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-04-22 00:34:29 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.cc
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -5,11 +5,7 @@ using namespace std;
 
 int n, i, m;
 vector<int> v;
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	
 	cin >> n; m = n;
 	while (n--) { cin >> i; v.push_back(i); }
 	sort(v.begin(), v.end());
