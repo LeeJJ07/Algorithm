80115566b1d7a90017f78d4ab0dd06d6c5d66211
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-07-24 08:40:05 +0900

    [Silver II] Title: 키로거, Time: 208 ms, Memory: 18740 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int t;
+    cin >> t;
+    
+    string str;
+   
+    while (t--)
+    {
+        cin >> str;
+        list<char> arrList;
+        auto it = arrList.begin();
+
+        for (char c : str)
+        {
+            if (c == '<')
+            {
+                if (it == arrList.begin()) continue;
+                it--;
+            }
+            else if (c == '>')
+            {
+                if (it == arrList.end()) continue;
+                it++;
+            }
+            else if (c == '-')
+            {
+                if (it == arrList.begin()) continue;
+                it = arrList.erase(--it);
+            }
+            else
+            {
+                it = arrList.insert(it, c);
+                it++;
+            }
+        }
+        for (char c : arrList)
+            cout << c;
+        cout << '\n';
+    }
+    return 0;
+}
