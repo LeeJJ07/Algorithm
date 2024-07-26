ae3ca1f3d9528c95b4feb1b7c5ba24b4264f9e0b
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-07-26 18:42:38 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -3,37 +3,39 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+int main()
+{
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin.tie(NULL); cout.tie(NULL);
 
-	vector<int> height(9);
 	int sum = 0;
-	for (int i = 0; i < 9; i++) {
-		cin >> height[i];
-		sum += height[i];
+	vector<int> arr(9);
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> arr[i];
+		sum += arr[i];
 	}
 	sum -= 100;
-	bool find = false;
-	vector<int> result;
-	for (int i = 0; i < 8; i++) {
-		for (int j = i + 1; j < 9; j++) {
-			if (height[i] + height[j] == sum) {
-				for (int k = 0; k < 9; k++) {
-					if (k == i || k == j)
-						continue;
-					result.push_back(height[k]);
-				}
-				find = true;
-				break;
+
+	vector<int> res;
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = i + 1; j < 9; j++)
+		{
+			if (arr[i] + arr[j] != sum) continue;
+			for (int k = 0; k < 9; k++)
+			{
+				if (k == i || k == j) continue;
+				res.push_back(arr[k]);
 			}
-		}
-		if (find)
 			break;
+		}
+		if (res.size()) break;
 	}
-	sort(result.begin(), result.end());
-	for (int i = 0; i < 7; i++) {
-		cout << result[i] << '\n';
-	}
+	sort(res.begin(), res.end());
+
+	for (int num : res)
+		cout << num << '\n';
+
+	return 0;
 }
