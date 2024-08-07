66218869fccd8d0e2cd2694be1e7a7a319b4c841
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-08-07 18:39:37 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -2,19 +2,18 @@
 #include <vector>
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+int main()
+{
+	vector<int> check(26);
 
 	string str;
 	cin >> str;
-	vector<int> A(26);
-	for (int i = 0; i < str.length(); i++) {
-		A[str[i] - 'a']++;
-	}
+
+	for (int i = 0; i < str.length(); i++)
+		check[str[i] - 'a']++;
 	
-	for (int i = 0; i < 26; i++) {
-		cout << A[i] << " ";
-	}
+	for (int i : check)
+		cout << i << ' ';
+
+	return 0;
 }
