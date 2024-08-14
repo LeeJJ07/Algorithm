9f4010dc56d9972398a3bac6f39be02f3c68b361
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2024-08-14 13:48:43 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,31 +1,40 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int n, x, a;
-vector<int> v;
-
-int main() {
+int main()
+{
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> a; v.push_back(a);
-	}
-	cin >> x;
-	sort(v.begin(), v.end());
-	int s = 0, e = v.size() - 1;
-	int ret = 0;
-	while (s < e) {
-		if (v[s] + v[e] == x) {
-			ret++;
-			s++; e--;
-		}
-		else if (v[s] + v[e] > x) e--;
-		else s++;
-	}
-	cout << ret << '\n';
+	cin.tie(NULL); cout.tie(NULL);
+
+    int N, X;
+    cin >> N;
+
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) cin >> arr[i];
+    cin >> X;
+
+    sort(arr.begin(), arr.end());
+
+    int s = 0, e = N - 1;
+
+    int res = 0;
+    while (s < e)
+    {
+        if (arr[s] + arr[e] == X)
+        {
+            s++;
+            e--;
+            res++;
+        }
+        else if (arr[s] + arr[e] > X)
+            e--;
+        else
+            s++;
+    }
+    cout << res << '\n';
+
+	return 0;
 }
