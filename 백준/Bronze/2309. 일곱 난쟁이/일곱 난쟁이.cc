5dd707a658b18ede423f18da712686be4f43f2f6
LeeJJ07 <125238027+LeeJJ07@users.noreply.github.com>
2025-08-06 16:17:46 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,41 +1,40 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+int sum = 0;
+vector<int> arr(9), res;
+
+void FindDwarfs() {
+	for (int i = 0; i < 8;i++) {
+		for (int j = i + 1;j < 9;j++) {
+			if (arr[i] + arr[j] != sum)
+				continue;
+			for (int k = 0; k < 9; k++) {
+				if (k == i || k == j)
+					continue;
+				res.push_back(arr[k]);
+			}
+			return;
+		}
+	}
+}
+
+int main() {
+	
+	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int sum = 0;
-	vector<int> arr(9);
-	for (int i = 0; i < 9; i++)
-	{
-		cin >> arr[i];
-		sum += arr[i];
+	for (int i = 0; i < 9;i++) {
+		cin >> arr[i]; sum += arr[i];
 	}
 	sum -= 100;
 
-	vector<int> res;
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = i + 1; j < 9; j++)
-		{
-			if (arr[i] + arr[j] != sum) continue;
-			for (int k = 0; k < 9; k++)
-			{
-				if (k == i || k == j) continue;
-				res.push_back(arr[k]);
-			}
-			break;
-		}
-		if (res.size()) break;
-	}
+	FindDwarfs();
 	sort(res.begin(), res.end());
 
-	for (int num : res)
-		cout << num << '\n';
-
-	return 0;
+	for (int r : res)
+		cout << r << '\n';
 }
